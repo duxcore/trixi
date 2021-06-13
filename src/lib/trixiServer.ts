68402e47415ba562e11398a53c29b12188836b5b
Jackson <jrobe3221@gmail.com>
2021-06-13 16:50:47 -0500

    added package.json and ws server constructor

--- src/lib/trixiServer.ts
+++ src/lib/trixiServer.ts
@@ -1,8 +1,11 @@
 import { TrixiServer, TrixiServerOptions } from "../types/TrixiServer";
+import { server as WebSocketServer} from 'websocket';
 
 export default function trixiServer({
   port
 }: TrixiServerOptions): TrixiServer {
+  const ws = new WebSocketServer();
+
   return {
     port,
   }
