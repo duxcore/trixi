084d00d27b084cf1572c6ec3e665d821f9ce3863
HoloPanio <jrobe3221@gmail.com>
2021-06-13 17:23:01 -0500

    setup createPayload helper and added payload types

--- src/helpers/createPayload.ts
+++ src/helpers/createPayload.ts
@@ -1,5 +1,10 @@
 import { PayloadObject } from "../types/Payload";
+import * as uuid from 'uuid';
 
-export default function createPayload(): PayloadObject {
-  return {};
+export default function createPayload(op, payload?, ref?): PayloadObject {
+  return {
+    op: op,
+    p: payload ?? null,
+    ref: ref ?? uuid.v4()
+  };
 }
--- src/types/Payload.ts
+++ src/types/Payload.ts
@@ -1,7 +1,14 @@
 export interface RawPayloadObject {
+  payload: PayloadObject,
+  meta: RawPayloadMeta
+}
 
+export interface RawPayloadMeta {
+  
 }
 
 export interface PayloadObject {
-  
+  op: string; // Operator Code
+  p: any | null; // Payload Data
+  ref: string; // Reference ID
 }
