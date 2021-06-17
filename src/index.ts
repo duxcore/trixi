3fa10ca0cddc813f22cd93e706eafd39b5d45aa0
Jackson <jrobe3221@gmail.com>
2021-06-17 13:00:29 -0500

    Exported Trixi Types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import trixi from "./trixi";
 
 export default trixi;
+
+export * from './types/Connection';
+export * from './types/Payload';
+
+export * from './types/Trixi';
+export * from './types/TrixiClient';
+export * from './types/TrixiServer';
